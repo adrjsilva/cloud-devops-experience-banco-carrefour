86cc457d291f5cc9ac2528f23a3494b844b22a39
adrjsilva <adrjsilva@gmail.com>
2022-10-07 12:21:14 -0300

    ajuste código script

--- user-dir-permissoes.sh
+++ user-dir-permissoes.sh
@@ -19,34 +19,34 @@ arraylengthgrupos=${#arraygrupos[@]}
 echo "Criando os Grupos"
 for (( i=1; i<${arraylengthgrupos}+1; i++));
 do
-    sudo groupadd $arraygrupos[$i-1]
+    groupadd ${arraygrupos[$i-1]}
 done
 
 echo "Criando os Diretórios e alterando suas permissões"
 for (( i=1; i<${arraylengthdir}+1; i++));
 do
-    sudo mkdir $arraydir[$i-1]
-    sudo chown root.$arraygrupos[$i-1]
-    sudo chmod 770 $arraydir[$i-1]
+    mkdir ${arraydir[$i-1]}
+    chown root.${arraygrupos[$i-1]}
+    chmod 770 ${arraydir[$i-1]}
 done
 
 echo "Criando usuários"
 for (( i=1; i<${arraylengthuser}+1; i++))
 do
-    sudo useradd -m $arrayuser[$i-1]
+    sudo useradd -m ${arrayuser[$i-1]}
 done
 
 echo "Adicionando aos grupos"
-    sudo usermod -aG GRP_ADM carlos
-    sudo usermod -aG GRP_ADM maria
-    sudo usermod -aG GRP_VEN debora
-    sudo usermod -aG GRP_VEN sebastiana
-    sudo usermod -aG GRP_SEC josefina
-    sudo usermod -aG GRP_SEC amanda
+    usermod -aG GRP_ADM carlos
+    usermod -aG GRP_ADM maria
+    usermod -aG GRP_VEN debora
+    usermod -aG GRP_VEN sebastiana
+    usermod -aG GRP_SEC josefina
+    usermod -aG GRP_SEC amanda
 
 echo "Criando pasta /public e setando permissóes para todos os usuários acessarem"
-    sudo mkdir /public
-    sudo chown 777 /public
+    mkdir /public
+    chown 777 /public
 
 
 
